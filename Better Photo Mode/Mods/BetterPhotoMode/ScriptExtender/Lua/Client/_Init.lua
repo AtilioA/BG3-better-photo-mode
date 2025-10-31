b97ae99519d24b89f67713c0aa894c40a1bce185
Atílio <27153776+AtilioA@users.noreply.github.com>
2025-10-31 13:14:38 -0300

    refactor: migrate `SetKeybindingCallback` to nested `Keybinding` namespace

--- Better Photo Mode/Mods/BetterPhotoMode/ScriptExtender/Lua/Client/_Init.lua
+++ Better Photo Mode/Mods/BetterPhotoMode/ScriptExtender/Lua/Client/_Init.lua
@@ -4,13 +4,13 @@ Ext.Events.SessionLoaded:Subscribe(function()
 end)
 
 -- Register keybinding callbacks for fast and slow modes
-MCM.SetKeybindingCallback("key_fast_mode", function()
+MCM.Keybinding.SetCallback("key_fast_mode", function()
     if MCM.Get("mod_enabled") then
         BPM.PhotoCamera.ToggleFastMode()
     end
 end)
 
-MCM.SetKeybindingCallback("key_slow_mode", function()
+MCM.Keybinding.SetCallback("key_slow_mode", function()
     if MCM.Get("mod_enabled") then
         BPM.PhotoCamera.ToggleSlowMode()
     end
